35e0671fc1a35da2d8fce424c904b2dd228f07f3
Adam Clarke <adamc@strategicdata.com.au>
2020-04-23 17:06:02 +1000

    Ensure publish target is correct.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,10 @@
 
 git remote | grep -q publish
 
-if [[ "$?" != "0" ]]; then
-  git remote add publish git@github.com:strategicdata/pmhc-mds-spec-wayback.git
+if [[ "$?" == "0" ]]; then
+  git remote remove publish
 fi
+
+git remote add publish git@github.com:strategicdata/pmhc-mds-spec-wayback.git
 
 git push publish
